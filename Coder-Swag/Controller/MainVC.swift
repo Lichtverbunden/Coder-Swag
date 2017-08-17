6a1d1a454190dce0eca97a689ecc01beec57bbc2
Ken Krippeler <kriken19@gmail.com>
2017-08-17 16:39:49 +0200

    TableView Basics added

--- Coder-Swag/Controller/MainVC.swift
+++ Coder-Swag/Controller/MainVC.swift
@@ -8,15 +8,39 @@
 
 import UIKit
 
-class MainVC: UIViewController
+class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource
 {
+    @IBOutlet weak var categoryTable: UITableView!
 
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        categoryTable.delegate = self
+        categoryTable.dataSource = self
         
     }
+    
+    func numberOfSections(in tableView: UITableView) -> Int
+    {
+        return 1
+    }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        
+    }
+    
+   
    
 
 
