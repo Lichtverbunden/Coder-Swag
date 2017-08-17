d3761d9a0bb3061cbeb91cc844cf76ec0a767e63
Ken Krippeler <kriken19@gmail.com>
2017-08-17 15:40:10 +0200

    First Setup

---
+++ Coder-Swag/Controller/MainVC.swift
@@ -0,0 +1,24 @@
+//
+//  MainVC.swift
+//  Coder-Swag
+//
+//  Created by Ken Krippeler on 17.08.17.
+//  Copyright © 2017 Lichtverbunden. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+    }
+
+   
+
+
+}
+
